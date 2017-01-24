345817da06c69c8e5d15381d534977d6fc5d29bc
abitofarson <libby.wichman@gmail.com>
2017-01-23 20:04:52 -0600

    Updated example.cpp
    Added variable "answer" 
    Added a last question
    Response to that question is assigned to answer

--- Example Program/example.cpp
+++ Example Program/example.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     // Variable declarations
-    string name, color;
+    string name, color, answer;
 
     // Output and input
     cout << "Hello, what is your name? ";
@@ -16,6 +16,9 @@ int main()
 
     cout << endl;
     cout << "So can I call you " << color << " " << name << "?" << endl;
+
+    cout << "Does that work for you? ";
+    cin >> answer;
 
     return 0;
 }
